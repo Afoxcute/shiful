3cade20fd8fad632ab6d895811f0a704b4a5eebe
Afoxcute <jwavolabilove16@gmail.com>
2025-04-10 21:37:54 +0100

    dummy

--- funmoney/src/constants/index.ts
+++
@@ -1,9 +0,0 @@
-import { abi as defaultAbi, contractAddress as defaultAddress, networkName as defaultNetworkName } from './contractInfo';
-
-export const getContractInfo = (chainId?: number) => {
-      return {
-        abi: defaultAbi,
-        contractAddress: defaultAddress,
-        networkName: defaultNetworkName
-      };
-}; 
